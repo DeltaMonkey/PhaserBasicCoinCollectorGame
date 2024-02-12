956a2abf7c805cecb4d9f98f8552cb4380ec9c71
DeltaMokey <engincanozkan@outlook.com>
2024-02-12 23:23:23 +0300

    Collide with the diamond

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import 'phaser';
 class PlayGame extends Phaser.Scene {
 
     private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-    private diamond: Phaser.Types.Physics.Arcade.SpriteWithStaticBody;
+    private diamond: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     private scoreText: Phaser.GameObjects.Text;
     private arrow:  Phaser.Types.Input.Keyboard.CursorKeys;
     private score: number;
@@ -16,7 +16,7 @@ class PlayGame extends Phaser.Scene {
 
     create() {
         this.player = this.physics.add.sprite(100, 100, 'player');
-        this.diamond = this.physics.add.staticSprite(300, 300, 'diamond');
+        this.diamond = this.physics.add.sprite(300, 300, 'diamond');
         
         // Display the score in the top left corner
         const scoreStyle: Phaser.Types.GameObjects.Text.TextStyle = { font: '20px Arial', color: '#fff',  };
@@ -41,6 +41,31 @@ class PlayGame extends Phaser.Scene {
         else if(this.arrow.up.isDown) {
             this.player.y -= 3;
         }
+
+        // If the player is overllaping with the diamond
+        if(this.physics.overlap(this.player, this.diamond)) {
+            this.hit();
+        } 
+    }
+
+    hit() {
+        // Change the position x and y of the diamond randomly
+        this.diamond.x = Phaser.Math.Between(100, 600);
+        this.diamond.y = Phaser.Math.Between(100, 300);
+
+        // Increment the score by 10
+        this.score += 10;
+
+        // Display the updated score on the screen
+        this.scoreText.setText('Score: ' + this.score);
+
+        this.tweens.add({
+            targets: this.player,
+            duration: 200,
+            scaleX: 1.2,
+            scaleY: 1.2,
+            yoyo: true
+        })
     }
 }
 
@@ -49,7 +74,7 @@ const GameConfig: Phaser.Types.Core.GameConfig = {
     height: 400,
     backgroundColor: '#3498db',
     scene: PlayGame, // The name of scene we created above
-    physics: { default: 'arcade' }, // The physics engine to use
+    physics: { default: 'arcade' /*, arcade: { debug: true } */ }, // The physics engine to use
     parent: 'thegame', // Create the game inside div in the index.html <div id='thegame'>,
 }
 
